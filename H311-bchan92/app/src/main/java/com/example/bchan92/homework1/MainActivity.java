badb6060e297ad6a77c14f865abe69f68bcf8ae4
Brian Chan <bchan92@uw.edu>
2015-04-05 11:37:58 -0700

    Added blue "lollipop" style theme

--- H311-bchan92/app/src/main/java/com/example/bchan92/homework1/MainActivity.java
+++ H311-bchan92/app/src/main/java/com/example/bchan92/homework1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bchan92.homework1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.topLevelToolBar);
+        setSupportActionBar(toolbar);
     }
 
 
