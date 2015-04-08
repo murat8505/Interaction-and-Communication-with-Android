da34d8637e394a30b27e27575d77962052f8f782
Brian Chan <bchan92@uw.edu>
2015-04-07 17:59:41 -0700

    Added some initial code for implementing the forecast

--- H311-bchan92/app/src/main/java/com/example/bchan92/homework1/fragment/ForecastFragment.java
+++ H311-bchan92/app/src/main/java/com/example/bchan92/homework1/fragment/ForecastFragment.java
@@ -1,107 +1,87 @@
 package com.example.bchan92.homework1.fragment;
 
 import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.example.bchan92.homework1.R;
 
 /**
- * Displays five day weather forecast in a list. Consists of the daily image, high/low temperatures
+ * Displays a five day weather forecast in a list. Consists of the daily image, high/low temperatures
  * and day of the week.
  */
 
-public class ForecastFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class ForecastFragment extends Fragment
+    implements AdapterView.OnItemClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String EXTRA_VALUES = ForecastFragment.class.getSimpleName() + ".values";
 
-    private OnFragmentInteractionListener mListener;
+    private int high_temp;
+    private int low_temp;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForecastFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ForecastFragment newInstance(String param1, String param2) {
-        ForecastFragment fragment = new ForecastFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private ForecastFragmentListener listener;
+
+    public interface ForecastFragmentListener {
+        public void onDaySelected();
     }
 
     public ForecastFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forecast, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        super();
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
+            listener = (ForecastFragmentListener) activity;
         }
+        catch (ClassCastException e) {
+            throw new ClassCastException(activity.getClass().getSimpleName()
+                    + " must implement ForecastFragmentListener");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // View holder here is not necessarily required since not accessing the
+        // list view after this method. But including for example purposes
+        ViewHolder holder = new ViewHolder(view);
+        view.setTag(holder);
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        listener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // open up the day detail fragment
+    }
+
+    private ViewHolder getViewHolder() {
+        View view = getView();
+        return view != null ? (ViewHolder) view.getTag() : null;
+    }
+
+    /* package */ static class ViewHolder {
+
+        ViewHolder(View view) {
+
+        }
     }
 
 }
