5e9d103cf9aff61a548e0800f6ede769117d7347
Brian Chan <bchan92@uw.edu>
2015-04-14 21:40:10 -0700

    Don't need menu for forecast

--- H311-bchan92/app/src/main/java/com/example/bchan92/homework1/activity/ForecastActivity.java
+++ H311-bchan92/app/src/main/java/com/example/bchan92/homework1/activity/ForecastActivity.java
@@ -9,10 +9,10 @@ import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 
 import com.example.bchan92.homework1.R;
-import com.example.bchan92.homework1.fragment.ForecastFragment;
+import com.example.bchan92.homework1.fragment.ForecastListFragment;
 
 public class ForecastActivity extends ActionBarActivity
-    implements ForecastFragment.ForecastFragmentListener {
+    implements ForecastListFragment.ForecastFragmentListener {
 
     public static Intent buildIntent(Context context) {
         return new Intent(context, ForecastActivity.class);
@@ -27,29 +27,6 @@ public class ForecastActivity extends ActionBarActivity
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_forecast, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
